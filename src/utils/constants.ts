88336aedb5ffffbe8d4ff8d88650cb916f6d18e4
Rtebbb <seabenkimsabenkim@gmail.com>
2026-04-30 13:54:09 +0300

    dev

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -629,6 +629,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "prism",
         id: 390884143749136386n,
     },
+    sikilmem: {
+        name: "sikilmem",
+        id: 1368308663504343041n,
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
